aa989ea2ee33a247e08f6002d095ef287661a216
ncx_official <nasikovskyi.v@gmail.com>
2024-01-29 15:03:09 -0800

    Created basic file structure of the app

---
+++ decrypt_data.py
@@ -0,0 +1,7 @@
+# from cryptography.hazmat.backends import default_backend
+# from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+# from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+# from cryptography.hazmat.primitives import hashes
+
+if __name__ == "__main__":
+    pass
---
+++ main.py
@@ -0,0 +1,8 @@
+import encrypt_data
+import decrypt_data
+
+def main():
+    return 0
+
+if __name__ == "__main__":
+    main()
